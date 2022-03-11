45e7f27e778c59baa3a865af42ed24cfa43d0b8f
Sebastian Rabiej <sebastian.rabiej@softwaremill.com>
2022-03-11 10:52:12 +0100

    init

---
+++ src/main/java/com/example/demo_renovate/DemoRenovateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_renovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRenovateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoRenovateApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/demo_renovate/DemoRenovateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_renovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRenovateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
